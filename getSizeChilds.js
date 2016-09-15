5a2290070e191b188bf2ac300c1774a9f9d7df27
иван-ПК\иван <wanes101@ya.ru>
2016-09-16 01:51:36 +0800

    no message

---
+++ getSizeChilds.js
@@ -0,0 +1,17 @@
+(function($){
+  /**
+   * Object:
+   *    width float
+   *    height float
+   * @returns Object
+   */
+  $.fn.getSizeChilds = function(){
+    var tar = this;
+    var height = 0.0,width = 0.0;
+    var childs = tar.children();
+    var wrap = childs.wrapAll('<div></div>').parent();
+    height = wrap.height();
+    width = wrap.width();
+    return {width:width,height:height};
+  };
+})(jQuery);
